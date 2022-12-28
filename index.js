97a1563602291e9851c6af8dad0d317e8f1cc52d
DanIeLLL21 <87934573+DanIeLLL21@users.noreply.github.com>
2022-12-28 12:09:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let diceContainer = document.getElementById('diceContainer')
+let diceContainer = document.querySelector('.diceContainer')
 let buttonContainer = document.createElement('div')
 
 let numberofrolls = document.getElementById('numofrolls')
@@ -7,8 +7,10 @@ let rollButton = document.getElementById('rollButton')
 let score = document.getElementById('score')
 let playAgainButton = document.getElementById('buttonPlayAgain')
 
-console.log(diceContainer.children)
 let leftoverDice = document.getElementById('leftoverDice')
+
+let allbuttons = document.getElementsByClassName('allbuttons')
+
 playAgainButton.disabled = false;
 
 let dices = 5;
@@ -26,29 +28,26 @@ for(let i = 0 ; i < dices ; i++) {
 
 	let roll = 	Math.floor(Math.random() * 6) + 1;
 	let rollText = document.createElement('button')
-	let bg = `images/dice${roll}.PNG`;
+	let bg = `images/dice${roll}.png`;
 
-	rollText.style.backgroundSize = "auto";
 	rollText.style.background = 'url(' + bg + ')';
-	rollText.style.backgroundRepeat = "no-repeat"
-	rollText.style.width = "200px"
-	rollText.style.height = "200px"
+	rollText.style.backgroundSize = "100% 100%";
 
 	rollText.backgroundImage = bg;
 	rollText.value = roll;
-	rollText.className = "a allbuttons"; 
+	rollText.className = "allbuttons"; 
 	diceContainer.appendChild(rollText);
 
 }
 
+
+
 let rolls = 0;
 
 rollButton.addEventListener('click', () => {
-	
-	numberofrolls.innerHTML = rolls;
 	rollDices();
-	rolls++
-
+	rolls++; 
+	numberofrolls.innerHTML = rolls;
 })
 
 
@@ -59,11 +58,11 @@ diceContainer.addEventListener('click', (e) => {
 		let points = Number(e.target.value);
 		roundTotal += points;
 
-		e.target.className = "b allbuttons";
+		e.target.style.backgroundColor ="black";
+		e.target.className = "allbuttons";
 
 		e.target.disabled = true;
 		playAgainButton.disabled = true;
-		
 
 		score.innerHTML = roundTotal;
 		leftDices.push(e.target.value)
@@ -71,38 +70,54 @@ diceContainer.addEventListener('click', (e) => {
 })
 
 
-playAgainButton.addEventListener('click', () => { resetDices();})
+playAgainButton.addEventListener('click', () => { resetDices(); })
+
 // ako je broj dajceva nula || broj rundi 3 onda play again button stavim da ponovim runde i vracam ih na nula.
-// uzeti zadnje buttone na tri rolla koji nisu disable i uzeti veljuje storat ih kao zadnje veljuje koje sam dobio na trecem rollu
 //dodati to sve na total koji sam upo svaki put kad sam disableo button
+
+// uzeti zadnje buttone na tri rolla koji nisu disable i uzeti veljuje storat ih kao zadnje veljuje koje sam dobio na trecem rollu
 
 dices = dices - leftDices.length;
 playAgainButton.disabled = true;
 
-
+// hitat stejt za end gejm i ne imati jebeni playagainbutton nego ako se hita stejt true ili false samo pozvat funkciju.
+// lupat se preko svih buttona i samo pitat koji je disabled koji nie cijelo cont disabled = true || false
 function rollDices () {
 
-		let enableButton = document.getElementsByClassName('a')
-
-		for(let i = 0 ; i < enableButton.length ; i++) {
-
+		for(let i = 0 ; i < allbuttons.length ; i++) {
 			let roll = 	Math.floor(Math.random() * 6) + 1;
-			enableButton[i].value = roll;
+			if(!allbuttons[i].disabled == true) {
+			allbuttons[i].value = roll;
 			let bg = `images/dice${roll}.png`;
-			enableButton[i].style.background = 'url(' + bg + ')';
-
+			allbuttons[i].style.background = 'url(' + bg + ')';
+			allbuttons[i].style.backgroundSize = "100% 100%";
+			 }
 		}
 
 		// if(dices == 0 || rolls == 3) {} countam broj svakog bacanja
 		// play again button za ponovni roll 		
 
-		if(rolls == 3 || disableCount == diceContainer.length) {
+		if(rolls == 2) {
 
 			rollButton.disabled = true;
 			playAgainButton.disabled = false;
+			getbuttonvalue();
+
 		}
 }
 
+
+function getbuttonvalue () {
+	
+	let score = 0;
+	let leftValues = [];
+
+
+	for(let i = 0 ; i < diceContainer.length ; i++) {
+		console.log(diceContainer[i].disabled)
+	}
+
+}
 
 
 function resetDices () {
@@ -116,16 +131,14 @@ function resetDices () {
 	numberofrolls.innerHTML = rolls;
 	score.innerHTML = roundTotal;
 
-	let albats = document.getElementsByClassName('allbuttons');
 
-	for(let i = 0 ; i < albats.length ; i++){
+	for(let i = 0 ; i < allbuttons.length ; i++){
 		let roll = 	Math.floor(Math.random() * 6) + 1;
 		let bg = `images/dice${roll}.png`;
-		let allbuttons = albats[i];
-		allbuttons.disabled = false;
-		allbuttons.value = roll;
-		allbuttons.className =	"a allbuttons";
-		allbuttons.style.background = 'url(' + bg + ')';
+		allbuttons[i].disabled = false;
+		allbuttons[i].value = roll;
+		allbuttons[i].style.background = 'url(' + bg + ')';
+		allbuttons[i].style.backgroundSize = "100% 100%";
 
 	}
 }
